87455fdf73947347dcfa6012f9c6ba1ce32dc14b
ebocher <erwan.bocher@univ-ubs.fr>
2016-09-30 11:15:07 +0200

    Prepare H2 Utils

--- RH2/src/main/R/h2.R
+++ RH2/src/main/R/h2.R
@@ -4,3 +4,8 @@ RH2 <- H2 <- function() {
 
 # load DBI to replicate GNU R 'Depends:'
 .onLoad <- function(libname, pkgname) library(DBI)
+
+
+dbListTables.JDBCConnection <- function(con) {
+	JDBCUtils$getTables(con$conn)	
+}
