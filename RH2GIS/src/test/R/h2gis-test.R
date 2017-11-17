a60445048cb64a78e07390d70e0e3969d823ec69
ebocher <erwan.bocher@univ-ubs.fr>
2017-11-17 10:09:23 +0100

    Cannot execute dbWriteTable

--- RH2GIS/src/test/R/h2gis-test.R
+++ RH2GIS/src/test/R/h2gis-test.R
@@ -13,6 +13,20 @@ test.driver <- function() {
         stopifnot(identical(as.numeric(dbGetQuery(con,"SELECT ST_AREA(the_geom) as area FROM VANNES")[[1]]),11000))
 }
 
+test.dbwriter <- function() {
+        dbPath = paste (tempdir(), "/h2gis", sep = "", collapse = NULL)
+        unlink(dbPath, recursive = FALSE, force = FALSE)
+        dbUrl = paste ("jdbc:h2:file:",dbPath,"/db_external", sep = "", collapse = NULL)
+        con <- dbConnect(RH2GIS(), url=dbUrl, username="sa", password="")
+	print(loadSpatialFunctions(con));
+        sq <- dbSendQuery(con,"DROP TABLE IF EXISTS VANNES; CREATE TABLE VANNES (the_geom geometry, id int)")
+        stopifnot(identical(dbExistsTable(con,"VANNES"),TRUE))
+        sq <- dbSendQuery(con,"INSERT INTO VANNES VALUES('POLYGON ((100 300, 210 300, 210 200, 100 200, 100 300))'::GEOMETRY, 1)"); 
+        selectedTable = dbGetQuery(con,"SELECT ST_AREA(the_geom) as area FROM VANNES");
+        dbWriteTable(con, "vannes_area", selectedTable, overwrite=TRUE);
+        stopifnot(identical(as.numeric(dbGetQuery(con,"SELECT ST_AREA(the_geom) as area FROM vannes_area")[[1]]),11000))
+}
+
 
 test.externalFile <- function() {        
         dbPath = paste (tempdir(), "/h2gis", sep = "", collapse = NULL)
